dbef859397c64718832b778fbfaaf64a47d4e829
Sonechka <148910070+demonsofya@users.noreply.github.com>
2026-09-14 15:06:07 +0300

    Добавлен lru-cache

--- cache.cpp
+++ cache.cpp
@@ -10,26 +10,30 @@
 using KeyT = size_t;
 
 size_t slow_get_value(KeyT key) {
-    
-    return key; // cause its not really important how is testing works
+
+  return key; // cause its not really important how is testing works
 }
 
 int main() {
 
-    size_t cache_sz = 0, data_count = 0; 
-    std::cin >> cache_sz >> data_count;
+  size_t cache_sz = 0, data_count = 0;
+  std::cin >> cache_sz >> data_count;
 
-    lfu_cache_t<size_t> cache(cache_sz);
+  lfu_cache_t<size_t> cache_lfu(cache_sz);
+  lru_cache_t<size_t> cache_lru(cache_sz);
 
-    size_t hits = 0;
-    for (int i = 0; i < data_count; i++) {
-        KeyT key;
-        std::cin >> key;
+  size_t hits_lfu = 0, hits_lru = 0;
+  for (int i = 0; i < data_count; i++) {
+    KeyT key;
+    std::cin >> key;
 
-        if (cache.lookup_update(key, slow_get_value))
-            hits++;
-    }
+    if (cache_lfu.lookup_update(key, slow_get_value))
+      hits_lfu++;
 
-    std::cout << hits << "\n";
-    return 0;
+    if (cache_lru.lookup_update(key, slow_get_value))
+      hits_lru++;
+  }
+
+  std::cout << "LFU hits: " << hits_lfu << "\nLRU hits: " << hits_lru << "\n";
+  return 0;
 }
--- cache.hpp
+++ cache.hpp
@@ -19,7 +19,7 @@
 
 template <typename T, typename KeyT = size_t> struct lfu_cache_t {
 
-  lfu_cache_t(size_t sz) : capacity_(sz) {}
+  lfu_cache_t(size_t capacity) : capacity_(capacity) {}
 
   // ~lfu_cache_t() = default;
 
@@ -48,16 +48,16 @@ template <typename T, typename KeyT = size_t> struct lfu_cache_t {
 
       if (next_it != counter_.end()) {
 
-ON_DEBAG(std::cout << "Founded element " << curr_it->first
-                   << "and swaped it with " << next_it->first << "\n";
-         print_cache(););
+        ON_DEBAG(std::cout << "Founded element " << curr_it->first
+                           << "and swaped it with " << next_it->first << "\n";
+                 print_cache(););
 
         std::swap(curr_it, next_it); // сортируем так чтобы было как надо,
                                      // просто свапаем тк разница максимум 1
       } else {
 
-ON_DEBAG(std::cout << "Founded element " << curr_it->first
-                   << " and didnt swaped\n");
+        ON_DEBAG(std::cout << "Founded element " << curr_it->first
+                           << " and didnt swaped\n");
       }
 
       return true;
@@ -72,8 +72,8 @@ ON_DEBAG(std::cout << "Founded element " << curr_it->first
 
       size_++;
 
-ON_DEBAG(std::cout << "Slowly got elem " << value << "\n";
-         print_cache(););
+      ON_DEBAG(std::cout << "Slowly got elem " << value << "\n";
+               print_cache(););
 
       return false;
     }
@@ -82,9 +82,9 @@ ON_DEBAG(std::cout << "Slowly got elem " << value << "\n";
     cache_.erase(deliting_elem->first);
 
     *deliting_elem = {key, static_cast<size_t>(1)}; // canging counter_
-    cache_[key] = value; // changing cache_
+    cache_[key] = value;                            // changing cache_
 
-ON_DEBAG(std::cout << "Slowly got elem " << value << "\n"; print_cache(););
+    ON_DEBAG(std::cout << "Slowly got elem " << value << "\n"; print_cache(););
 
     return false;
   }
@@ -101,7 +101,7 @@ ON_DEBAG(std::cout << "Slowly got elem " << value << "\n"; print_cache(););
 
     std::cout << "\nPRINTING CACHE_\n";
     for (auto it = cache_.begin(); it != cache_.end(); it++) {
-        std::cout << it->second << "\n";
+      std::cout << it->second << "\n";
     }
 
     std::cout << "----------------------\n\n";
@@ -117,4 +117,80 @@ ON_DEBAG(std::cout << "Slowly got elem " << value << "\n"; print_cache(););
 
   // эта штука хранит количество обращений по ключам - они по ключам
   // отсортированны
+};
+
+//==================================================================
+
+template <typename T, typename KeyT = size_t> struct lru_cache_t {
+
+  lru_cache_t(size_t capacity) : capacity_(capacity) {}
+
+  // ~lru_cache_t() = default;
+
+  template <typename F> bool lookup_update(KeyT key, F slow_get_value) {
+
+    if (capacity_ == 0)
+      return false;
+
+    auto cache_iter = cache_.find(key);
+    if (cache_iter != cache_.end()) {
+
+      auto counter_iter = cache_iter->second;
+      counter_.splice(counter_.begin(), counter_, counter_iter);
+
+      ON_DEBAG(std::cout << "Founeded elem " << counter_iter->first << "\n";
+               print_cache(););
+
+      return true;
+    }
+
+    T value = slow_get_value(key);
+    if (size_ == capacity_) {
+
+      cache_.erase(counter_.back().first);
+
+      ON_DEBAG(std::cout << "Deleting from counter and cache elemet "
+                         << counter_.begin()->first << "\n");
+
+      counter_.pop_back(); // last recently used element
+
+    } else {
+
+      size_++;
+    }
+
+    counter_.emplace_front(std::make_pair(key, value));
+    cache_[key] = counter_.begin();
+
+    ON_DEBAG(std::cout << "Slowly got elem " << value << "\n"; print_cache(););
+
+    return false;
+  }
+
+  void print_cache() {
+
+    std::cout << "\n\nPRINTING COUNTER_\n"
+              << "cache size is " << size_ << " and capacity is " << capacity_
+              << "\n";
+
+    for (auto it = counter_.begin(); it != counter_.end(); it++) {
+      std::cout << it->first << " value " << it->second << "\n";
+    }
+
+    std::cout << "\nPRINTING CACHE_\n";
+    for (auto it = cache_.begin(); it != cache_.end(); it++) {
+      std::cout << it->second->first << " value " << it->second->second << "\n";
+    }
+
+    std::cout << "----------------------\n\n";
+  }
+
+  size_t size_ = 0; // текущий размер всего
+  size_t capacity_;
+
+  std::list<std::pair<KeyT, T>> counter_; // сортируем чтобы в начале лежал
+  // тот, который хотим удалить
+  using CounterIter = typename std::list<std::pair<KeyT, size_t>>::iterator;
+
+  std::unordered_map<KeyT, CounterIter> cache_; // похуй на сортировку
 };
