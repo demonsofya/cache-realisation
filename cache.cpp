ccd7c30fb8fa57c6d6d14a526ee19195fc20aec8
Sonechka <148910070+demonsofya@users.noreply.github.com>
2026-09-14 14:12:12 +0300

    Добавлен cmake

--- cache.cpp
+++ cache.cpp
@@ -10,10 +10,12 @@
 using KeyT = size_t;
 
 size_t slow_get_value(KeyT key) {
+    
     return key; // cause its not really important how is testing works
 }
 
 int main() {
+
     size_t cache_sz = 0, data_count = 0; 
     std::cin >> cache_sz >> data_count;
 
--- cache.hpp
+++ cache.hpp
@@ -24,6 +24,7 @@ template <typename T, typename KeyT = size_t> struct lfu_cache_t {
   // ~lfu_cache_t() = default;
 
   template <typename F> bool lookup_update(KeyT key, F slow_get_value) {
+
     if (capacity_ == 0) {
       return false;
     }
@@ -46,27 +47,34 @@ template <typename T, typename KeyT = size_t> struct lfu_cache_t {
         next_it++;
 
       if (next_it != counter_.end()) {
-        ON_DEBAG(std::cout << "Founded element " << curr_it->first
-                           << "and swaped it with " << next_it->first << "\n";
-                 print_cache(););
+
+ON_DEBAG(std::cout << "Founded element " << curr_it->first
+                   << "and swaped it with " << next_it->first << "\n";
+         print_cache(););
+
         std::swap(curr_it, next_it); // сортируем так чтобы было как надо,
                                      // просто свапаем тк разница максимум 1
       } else {
-        ON_DEBAG(std::cout << "Founded element " << curr_it->first
-                           << " and didnt swaped\n");
+
+ON_DEBAG(std::cout << "Founded element " << curr_it->first
+                   << " and didnt swaped\n");
       }
+
       return true;
     }
 
     T value = slow_get_value(key);
     if (size_ < capacity_) {
+
       cache_[key] = value; // сразу фигачим элемент в кеш
       counter_.emplace_front(std::make_pair(
           key, static_cast<size_t>(1))); // добавяем счетчик одного обращения
 
       size_++;
-      ON_DEBAG(std::cout << "Slowly got elem " << value << "\n";
-               print_cache(););
+
+ON_DEBAG(std::cout << "Slowly got elem " << value << "\n";
+         print_cache(););
+
       return false;
     }
 
@@ -76,14 +84,17 @@ template <typename T, typename KeyT = size_t> struct lfu_cache_t {
     *deliting_elem = {key, static_cast<size_t>(1)}; // canging counter_
     cache_[key] = value; // changing cache_
 
-    ON_DEBAG(std::cout << "Slowly got elem " << value << "\n"; print_cache(););
+ON_DEBAG(std::cout << "Slowly got elem " << value << "\n"; print_cache(););
+
     return false;
   }
 
   void print_cache() {
-    std::cout << "\n\nPRINTING COUNTER_\n";
-    std::cout << "cache size is " << size_ << " and capacity is " << capacity_
+
+    std::cout << "\n\nPRINTING COUNTER_\n"
+              << "cache size is " << size_ << " and capacity is " << capacity_
               << "\n";
+
     for (auto it = counter_.begin(); it != counter_.end(); it++) {
       std::cout << it->first << " freq " << it->second << "\n";
     }
@@ -98,10 +109,12 @@ template <typename T, typename KeyT = size_t> struct lfu_cache_t {
 
   size_t size_ = 0; // текущий размер всего
   size_t capacity_;
+
   std::unordered_map<KeyT, T> cache_; // похуй на сортировку
   std::list<std::pair<KeyT, size_t>>
       counter_; // сортируем по ключам чтобы в начале лежал тот, который хотим
                 // удалить
+
   // эта штука хранит количество обращений по ключам - они по ключам
   // отсортированны
 };
